a8e1fbe12f9a45355096a2a7133a81b4b526bce5
Adam Smith <alice@Adams-MacBook-Pro.local>
2025-01-15 09:46:55 +0000

    update the module

--- network/build/network.tf
+++ network/build/network.tf
@@ -1,4 +1,18 @@
 resource "azurerm_resource_group" "wayfinder" {
-  name     = var.resource_group_name
+  name     = var.resource_group
   location = var.location
+}
+
+resource "azurerm_virtual_network" "wayfinder" {
+  name = var.cloud_resource
+  location = var.location
+  resource_group_name = azurerm_resource_group.wayfinder.name
+  address_space = [var.cidr_node]
+}
+
+resource "azurerm_subnet" "wayfinder" {
+  name                 = "${var.cloud_resource}-nodes"
+  resource_group_name  = azurerm_resource_group.wayfinder.name
+  virtual_network_name = azurerm_virtual_network.wayfinder.name
+  address_prefixes     = [var.cidr_node]
 }
--- network/build/variables.tf
+++ network/build/variables.tf
@@ -1,4 +1,14 @@
-variable "resource_group_name" {
+variable "cloud_resource" {
+  description = "blank"
+  type = string
+}
+
+variable "cidr_node" {
+  description = "blank"
+  type = string
+}
+
+variable "resource_group" {
   description = "blank"
   type        = string
 }
@@ -6,4 +16,14 @@ variable "resource_group_name" {
 variable "location" {
   description = "blank"
   type        = string
+}
+
+variable "layout_mode" {
+  description = "blank"
+  type = string
+}
+
+variable "layout_private_subnets" {
+  description = "blank"
+  type = number
 }
