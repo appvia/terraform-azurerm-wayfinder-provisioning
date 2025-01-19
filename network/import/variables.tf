a3e159a99867e7ff49b36bf1b7d49060300cd67c
Adam Smith <alice@Adams-MacBook-Pro.local>
2025-01-19 19:20:52 +0000

    byo module

---
+++ network/import/variables.tf
@@ -0,0 +1,19 @@
+variable "existing_network" {
+  description = "blank"
+  type = string
+}
+
+variable "cloud_resource" {
+  description = "blank"
+  type = string
+}
+
+variable "cidr_node" {
+  description = "blank"
+  type = string
+}
+
+variable "resource_group" {
+  description = "blank"
+  type        = string
+}
