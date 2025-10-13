710a2b32ed336aaa71914a038c6c5a743867629a
Adam Smith <alice@Adams-MacBook-Pro.local>
2025-10-13 12:43:37 +0100

    azuread client

--- cluster/build/main.tf
+++ cluster/build/main.tf
@@ -2,6 +2,8 @@ data "azurerm_subscription" "primary" {}
 
 data "azurerm_client_config" "current" {}
 
+data "azuread_client_config" "current" {}
+
 locals {
 	resource_group = var.resource_group == "" ? "wf-${var.cloud_resource}-${var.location}" : var.resource_group
 
